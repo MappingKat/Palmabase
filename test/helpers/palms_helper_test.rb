61b19ee8aae9736de8ab6d0c631f004c327f0b7e
Katrina Engelsted <katrina@engelsted.co>
2013-11-08 22:06:12 -0700

    deleting test and unnecessary files

--- test/helpers/palms_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PalmsHelperTest < ActionView::TestCase
-end
--- test/models/palm_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PalmTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
